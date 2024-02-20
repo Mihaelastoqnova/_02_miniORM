2a8cab1cfdbdc38fac4bf5dd6c1927e9042d7124
Mihaelastoqnova <122827141+Mihaelastoqnova@users.noreply.github.com>
2024-02-21 00:17:19 +0200

    Create DatabaseContext.java

---
+++ src/main/java/orm/DatabaseContext.java
@@ -0,0 +1,13 @@
+package orm;
+
+import java.sql.SQLException;
+
+public interface DatabaseContext<E> {
+    boolean persist(E entity) throws SQLException, IllegalAccessException;
+
+    Iterable<E> find(Class<E> table);
+    Iterable<E> find(Class<E> table, String where);
+
+    E findFirst(Class<E> table);
+    E findFirst(Class<E> table, String where);
+}
