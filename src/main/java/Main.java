090c7ccd5f5b1ed3a077ad100b71c32b97650aca
Mihaelastoqnova <122827141+Mihaelastoqnova@users.noreply.github.com>
2024-02-20 21:24:50 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,18 @@
+import entities.User;
+import orm.EntityManager;
 import orm.MyConnector;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
         MyConnector.createConnection("root", "12345", "mini_orm");
         Connection connection = MyConnector.getConnection();
+
+        EntityManager<User> userEntityManager = new EntityManager<>(connection);
+        User pesho = new User("pesho", 43, LocalDate.now());
+        userEntityManager.persist(pesho);
     }
 }
