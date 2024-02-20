510f365be8b63bb3ded79fb93cd457678dd0048e
Mihaelastoqnova <122827141+Mihaelastoqnova@users.noreply.github.com>
2024-02-20 11:03:49 +0200

    Update User.java

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -1,5 +1,41 @@
 package entities;
 
-public class User {
+import java.time.LocalDate;
 
+public class User {
+    private long id;
+    private String username;
+    private int age;
+
+    private LocalDate registration;
+
+    public User(String username, int age, LocalDate registration){
+        this.username = username;
+        this.age = age;
+        this.registration = registration;
+    }
+    public long getId(){
+        return id;
+    }
+    public void setId(long id){
+        this.id = id;
+    }
+    public String username(){
+        return username;
+    }
+    public void setUsername(String username){
+        this.username = username;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public LocalDate getRegistration(){
+        return registration;
+    }
+    public void setRegistration(LocalDate registration){
+        this.registration = registration;
+    }
 }
