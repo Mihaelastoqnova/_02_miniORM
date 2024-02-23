0b6a2b737864b9f481aeeb970f1c06d69596c4ed
Mihaelastoqnova <122827141+Mihaelastoqnova@users.noreply.github.com>
2024-02-23 13:51:37 +0200

    Update DatabaseContext.java

--- src/main/java/orm/DatabaseContext.java
+++ src/main/java/orm/DatabaseContext.java
@@ -1,12 +1,13 @@
 package orm;
 
+import java.lang.reflect.InvocationTargetException;
 import java.sql.SQLException;
 
 public interface DatabaseContext<E> {
     boolean persist(E entity) throws SQLException, IllegalAccessException;
 
-    Iterable<E> find(Class<E> table);
-    Iterable<E> find(Class<E> table, String where);
+    Iterable<E> find(Class<E> table) throws SQLException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException;
+    Iterable<E> find(Class<E> table, String where) throws SQLException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException;
 
     E findFirst(Class<E> table);
     E findFirst(Class<E> table, String where);
