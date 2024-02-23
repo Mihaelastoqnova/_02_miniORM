225fa22f847398d51f74b741b6280561599af314
Mihaelastoqnova <122827141+Mihaelastoqnova@users.noreply.github.com>
2024-02-23 13:51:03 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,17 +2,19 @@ import entities.User;
 import orm.EntityManager;
 import orm.MyConnector;
 
+import java.lang.reflect.InvocationTargetException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.time.LocalDate;
 
 public class Main {
-    public static void main(String[] args) throws SQLException, IllegalAccessException {
+    public static void main(String[] args) throws SQLException, IllegalAccessException, InvocationTargetException, NoSuchMethodException, InstantiationException {
         MyConnector.createConnection("root", "12345", "mini_orm");
         Connection connection = MyConnector.getConnection();
 
         EntityManager<User> userEntityManager = new EntityManager<>(connection);
         User pesho = new User("pesho", 43, LocalDate.now());
+        pesho.setId(1);
 
         userEntityManager.persist(pesho);
 
